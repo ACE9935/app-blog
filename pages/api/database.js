7f590b45803d89353d678c95dc864827e92501f4
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-02-20 15:17:34 +0100

    update y

--- pages/api/database.js
+++ pages/api/database.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const pwd = process.env.PWD;
+const encodedPwd = encodeURIComponent(pwd);
  
   // Replace '<your_connection_string>' with your MongoDB Atlas connection string
   const uri = "mongodb+srv://admin:"+pwd+"@cluster0.i5dwy.mongodb.net/myDB?retryWrites=true&w=majority";
