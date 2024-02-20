cc151f6548801edd135448372fa6f8d8cb5688e0
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-02-20 15:35:54 +0100

    css update

--- components/Articles/TitleSection.js
+++ components/Articles/TitleSection.js
@@ -4,8 +4,8 @@ import ATitle from "../../components/Articles/ATitle";
 function TitleSection({title,date,sub}) {
     return ( 
         <div>
-           <Stext className=" font-[800] pb-1 text-3xl md:text-4xl text-black" tag={"p"} x={1} y={1} color={"white"}>{sub}</Stext>
-           <ATitle className="font-[300] text-slate-100 text-3xl md:text-5xl pb-3 md:!leading-[54px]">{title}</ATitle>
+           <Stext className=" font-[800] pb-2 text-3xl md:text-4xl text-black" tag={"p"} x={1} y={1} color={"white"}>{sub}</Stext>
+           <ATitle className="font-[300] text-white text-3xl md:text-5xl pb-4 md:!leading-[54px]">{title}</ATitle>
            <p className="text-slate-300 text-md md:text-2xl ">{date}</p>
            </div>
      );
