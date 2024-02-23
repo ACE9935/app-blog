32f84bd09106c26d0469cb5661f07c70da80b9a0
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-02-23 01:15:20 +0100

    y

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -62,8 +62,7 @@ function Articles({data,article}) {
     return ( 
         <section className='w-full relative bg-primary py-12 flex justify-center'>
          <Head>
-        <title>{article.title}</title>
-        <meta name="description" content="I post all type of content on this blog." />
+        
       </Head>
            <div className="z-[5] relative px-3 sm:px-6 py-4 sm:py-8 text-[1.35rem] leading-[1.625] flex flex-col gap-[2rem] max-w-[1400px]">
             <TitleSection date={article.date} title={article.title} sub={article.genre}/>
