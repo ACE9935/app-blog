9328c5f167c40e328b57e204741901412457dffa
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-03-29 17:41:56 +0100

    fix third home section gallery

--- components/Home/GenreTab.js
+++ components/Home/GenreTab.js
@@ -6,7 +6,7 @@ function GenreTab({data,genre}) {
 
 
     return ( 
-        <div className={`${styles.tabg} relative tabg-ani gap-6 sm:py-6 sm:px-24 items-center flex`}>
+        <div className={`${styles.tabg} relative tabg-ani gap-6 sm:py-6 sm:px-24 items-center flex max-w-[1400px]`}>
         <div className={styles["comic-book-page"]}><GenreGall data={data} />
         <span className={styles["borderx-gally"]}></span>
         </div>
